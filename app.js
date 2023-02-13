d0078e3e675290eae635cdcb9646e41daa28c570
faisal-raza-se <71428413+faisal-raza-se@users.noreply.github.com>
2023-02-13 02:48:00 +0000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,60 @@
+// Listen to Submit
+document.getElementById('loan-form').addEventListener('submit', function(e){
+    //Hide Results
+    document.getElementById('results').style.display='none';
+    setTimeout(calculteResults, 2000);
+
+    // Show Results
+    document.getElementById('loading').style.display ='block';
+    e.preventDefault();
+});
+
+//Calculate Results
+function calculteResults(){
+    //UI Variables
+    const amount = document.getElementById('amount');
+    const interest = document.getElementById('interest');
+    const years = document.getElementById('years');
+    const monthlyPayment = document.getElementById('monthly-payment');
+    const totalPayment = document.getElementById('total-payment');
+    const totalInterest = document.getElementById('total-interest');
+
+    const principal = parseFloat(amount.value);
+    const calculatedInterest = parseFloat(interest.value) / 100 /12;
+    const calculatedPayments = parseFloat(years.value) * 12;
+
+    // Compute Monthly payments
+    const x = Math.pow(1 + calculatedInterest, calculatedPayments);
+    const monthly = (principal*x*calculatedInterest)/(x-1);
+
+    if(isFinite(monthly)) {
+        monthlyPayment.value = monthly.toFixed(2);
+        totalPayment.value = (monthly*calculatedPayments).toFixed();
+        totalInterest.value = ((monthly*calculatedPayments)-principal).toFixed();
+        document.getElementById('results').style.display='block';
+        document.getElementById('loading').style.display='none';
+
+    } else {
+        showError('Please check your numbers');
+    }
+
+    
+}
+
+//Show Error
+function showError(error) {
+    const errorDiv = document.createElement('div');
+    const card = document.querySelector('.card');
+    const heading = document.querySelector('.heading');
+    errorDiv.className = 'alert alert-danger';
+
+    errorDiv.appendChild(document.createTextNode(error));
+
+    card.insertBefore(errorDiv, heading);
+
+    // Clear Error after 3 seconds
+    setTimeout(clearError, 3000);
+}
+function clearError(){
+    document.querySelector('.alert').remove();
+}
